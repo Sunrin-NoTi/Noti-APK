6f63e123b8087b1838592f77ae2caa2c44b569b1
blgada12 <blgada12@naver.com>
2017-09-05 11:37:04 +0900

    backpress_clear

--- app/src/main/java/com/noti/sns/MainActivity.kt
+++ app/src/main/java/com/noti/sns/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.noti.sns
 
 import android.content.Intent
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
+import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_register.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,17 +18,33 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+
+
         login_btn.setOnClickListener {
             val intent_login = Intent(this, MenuActivity::class.java)
             startActivity(intent_login)
         }
+        login_btn.setOnTouchListener({ view, motionEvent ->
+            Btn_press().bigBTN(motionEvent,login_btn)
+        })
 
         register_btn.setOnClickListener{
             val intent_register = Intent(this, RegisterActivity::class.java)
             startActivity(intent_register)
         }
+        register_btn.setOnTouchListener({ view, motionEvent ->
+            Btn_press().smallBTN(motionEvent,register_btn)
+        })
 
+        passwd_btn.setOnClickListener {
+
+        }
+        passwd_btn.setOnTouchListener({ view, motionEvent ->
+            Btn_press().smallBTN(motionEvent,passwd_btn)
+        })
     }
+
+
 
     override fun onBackPressed() {
         super.onBackPressed()
--- app/src/main/java/com/noti/sns/RegisterActivity.kt
+++ app/src/main/java/com/noti/sns/RegisterActivity.kt
@@ -4,12 +4,20 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        register_confirm.setOnClickListener{
+
+        }
+        register_confirm.setOnTouchListener({ view, motionEvent ->
+            Btn_press().bigBTN(motionEvent,register_confirm)
+        })
 
 
     }
