f45cf77af866adb0ee4822eaa46837d02cbb6be4
Shin-JaeHeon <heoni0504@gmail.com>
2017-10-29 21:20:48 +0900

    알림

--- app/src/main/java/com/noti/sns/fcm/MyFirebaseMessagingService.java
+++ app/src/main/java/com/noti/sns/fcm/MyFirebaseMessagingService.java
@@ -1,27 +1,29 @@
 package com.noti.sns.fcm;
 
-import android.util.Log;
-
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
-import static android.content.ContentValues.TAG;
+import java.util.Map;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 	@Override
 	public void onMessageReceived(RemoteMessage remoteMessage) {
-		// ...
+		Map<String,String> data = remoteMessage.getData();
+		if (data.size() > 0) {
+			if(data.containsKey("type")) {
+				if (data.get("type").equals("B")) { //아침 알람
 
-		// TODO(developer): Handle FCM messages here.
-		// Not getting messages here? See why this may be: https://goo.gl/39bRNJ
-		Log.d(TAG, "From: " + remoteMessage.getFrom());
+				} else if (data.get("type").equals("L")) { //점심 알람
 
-		// Check if message contains a data payload.
-		if (remoteMessage.getData().size() > 0) {
-			Log.d(TAG, "Message data payload: " + remoteMessage.getData());
-			/* 알림 작성 */
+				} else if (data.get("type").equals("D")) { //저녁 알람
 
-
+				}
+			}else if(data.containsKey("month")){
+				int month =  Integer.valueOf(data.get("month"));
+				int date =  Integer.valueOf(data.get("date"));
+				String title = remoteMessage.getNotification().getTitle();
+				String msg = remoteMessage.getNotification().getBody();
+			}
 		}
 
 	}
