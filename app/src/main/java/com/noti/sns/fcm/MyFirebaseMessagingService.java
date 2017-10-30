7bbc35b2dd35ad9584db173fbe70f996c5e5eb4e
Shin-JaeHeon <heoni0504@gmail.com>
2017-10-30 23:55:55 +0900

    Merge branch 'master' of C:\Users\Shin-JaeHeon\OneDrive\Develop\Remastering with conflicts.

--- app/src/main/java/com/noti/sns/fcm/MyFirebaseMessagingService.java
+++ app/src/main/java/com/noti/sns/fcm/MyFirebaseMessagingService.java
@@ -12,7 +12,6 @@ import android.os.Build;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
-import com.noti.sns.R;
 import com.noti.sns.activity.MainActivity;
 import com.noti.sns.schoolparsing.SchoolMenu;
 import com.noti.sns.utility.Listsave;
@@ -22,71 +21,70 @@ import java.util.List;
 import java.util.Map;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
-    @Override
-    public void onMessageReceived(RemoteMessage remoteMessage) {
-        Date today = new Date();
-        Map<String, String> data = remoteMessage.getData();
-        if (data.size() > 0) {
-            if (data.containsKey("type")) {
-                NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-                Intent intent = new Intent(this, MainActivity.class);
-                PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-                Notification.Builder builder;
-                String meal[] = new String[0];
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    NotificationChannel notificationChannel = new NotificationChannel("my_notification_channel", "My Notifications", NotificationManager.IMPORTANCE_DEFAULT);
-                    notificationChannel.setDescription("Channel description");
-                    notificationChannel.enableLights(true);
-                    notificationChannel.setLightColor(Color.RED);
-                    notificationManager.createNotificationChannel(notificationChannel);
-                    builder = new Notification.Builder(this, "my_notification_channel");
-                } else {
-                    builder = new Notification.Builder(this);
-                }
-                List<SchoolMenu> ex = Listsave.SaveSchool.get_meal_month(today.getMonth() + 1);
+	@Override
+	public void onMessageReceived(RemoteMessage remoteMessage) {
+		Date today = new Date();
+		Map<String, String> data = remoteMessage.getData();
+		if (data.size() > 0) {
+			if (data.containsKey("type")) {
+				NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+				Intent intent = new Intent(this, MainActivity.class);
+				PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+				Notification.Builder builder;
+				String meal[] = new String[0];
+				if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+					NotificationChannel notificationChannel = new NotificationChannel("my_notification_channel", "My Notifications", NotificationManager.IMPORTANCE_DEFAULT);
+					notificationChannel.setDescription("Channel description");
+					notificationChannel.enableLights(true);
+					notificationChannel.setLightColor(Color.RED);
+					notificationManager.createNotificationChannel(notificationChannel);
+					builder = new Notification.Builder(this, "my_notification_channel");
+				} else {
+					builder = new Notification.Builder(this);
+				}
+				List<SchoolMenu> ex = Listsave.SaveSchool.get_meal_month(today.getMonth() + 1);
 
-                builder.setSmallIcon(android.R.drawable.star_on);
-                builder.setContentText("식단");
-                builder.setWhen(System.currentTimeMillis());
-                builder.setSmallIcon(R.mipmap.ic_launcher);
-                builder.setContentIntent(pendingIntent);
-                builder.setAutoCancel(true);
-                if (data.get("type").equals("B")) { //아침 알람
+				builder.setSmallIcon(android.R.drawable.star_on);
+				builder.setContentText("식단");
+				builder.setWhen(System.currentTimeMillis());
+				builder.setContentIntent(pendingIntent);
+				builder.setAutoCancel(true);
+				if (data.get("type").equals("B")) { //아침 알람
 
-                    builder.setContentTitle("조식 알림");
-                    meal = ex.get(today.getDate() - 1).lunch.split("dses\n");
+					builder.setContentTitle("조식 알림");
+					meal = ex.get(today.getDate() - 1).lunch.split("dses\n");
 
-                } else if (data.get("type").equals("L")) { //점심 알람
+				} else if (data.get("type").equals("L")) { //점심 알람
 
-                    builder.setContentTitle("중식 알림");
-                    meal = ex.get(today.getDate() - 1).lunch.split("\n");
+					builder.setContentTitle("중식 알림");
+					meal = ex.get(today.getDate() - 1).lunch.split("\n");
 
-                } else if (data.get("type").equals("D")) { //저녁 알람
-                    builder.setContentTitle("석식 알림");
-                    meal = ex.get(today.getDate() - 1).lunch.split("\n");
+				} else if (data.get("type").equals("D")) { //저녁 알람
+					builder.setContentTitle("석식 알림");
+					meal = ex.get(today.getDate() - 1).lunch.split("\n");
 
-                }
-                Notification.InboxStyle inboxStyle = null;
-                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN) {
-                    inboxStyle = new Notification.InboxStyle(builder);
+				}
+				Notification.InboxStyle inboxStyle = null;
+				if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN) {
+					inboxStyle = new Notification.InboxStyle(builder);
 
-                    for (int i = 0; i < meal.length; i++)
-                        inboxStyle.addLine(meal[i]);
-                    inboxStyle.setSummaryText("전체 보기");
-                    builder.setStyle(inboxStyle);
-                    notificationManager.notify(0, builder.build());
-                }
+					for (int i = 0; i < meal.length; i++)
+						inboxStyle.addLine(meal[i]);
+					inboxStyle.setSummaryText("전체 보기");
+					builder.setStyle(inboxStyle);
+					notificationManager.notify(0, builder.build());
+				}
 
 
-            } else if (data.containsKey("month")) {// 일정알람
-                String year = data.get("year"); // 김도현년
-                String month = data.get("month").length()<10?"0"+data.get("month"):data.get("month"); // 월
-                String date = data.get("date").length()<10?"0"+data.get("date"):data.get("date"); // 월 // 일
-                String title = remoteMessage.getNotification().getTitle(); //제목
-                String msg = remoteMessage.getNotification().getBody(); //메세지
-                Listsave.HomeCardList.add(title,year+"."+month+"."+date+".",msg);
-            }
-        }
+			} else if (data.containsKey("month")) {// 일정알람
+				String year = data.get("year"); // 년
+				String month = data.get("month").length() < 10 ? "0" + data.get("month") : data.get("month"); // 월
+				String date = data.get("date").length() < 10 ? "0" + data.get("date") : data.get("date"); // 월 // 일
+				String title = remoteMessage.getNotification().getTitle(); //제목
+				String msg = remoteMessage.getNotification().getBody(); //메세지
+				Listsave.HomeCardList.add(title, year + "." + month + "." + date + ".", msg);
+			}
+		}
 
-    }
+	}
 }
