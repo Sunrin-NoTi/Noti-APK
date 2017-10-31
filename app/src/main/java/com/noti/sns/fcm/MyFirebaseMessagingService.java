7f511e61cdba1364da96580003e880c5fd4e2abe
blgada12 <blgada12@naver.com>
2017-10-31 16:58:51 +0900

    마지막 코드다듬기

--- app/src/main/java/com/noti/sns/fcm/MyFirebaseMessagingService.java
+++ app/src/main/java/com/noti/sns/fcm/MyFirebaseMessagingService.java
@@ -9,7 +9,6 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
-import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -23,97 +22,95 @@ import java.util.List;
 import java.util.Map;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
-	@Override
-	public void onMessageReceived(RemoteMessage remoteMessage) {
-		Date today = new Date();
-		Map<String, String> data = remoteMessage.getData();
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        Date today = new Date();
+        Map<String, String> data = remoteMessage.getData();
 
-		Log.e("e", remoteMessage.getNotification().getBody());
-		if (data.size() > 0) {
-			if (data.containsKey("type")) {
-				Log.e("e", data.get("type"));
-				NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-				Intent intent = new Intent(this, MainActivity.class);
-				PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-				Notification.Builder builder;
-				String meal[] = new String[0];
-				if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-					NotificationChannel notificationChannel = new NotificationChannel("my_notification_channel", "My Notifications", NotificationManager.IMPORTANCE_DEFAULT);
-					notificationChannel.setDescription("Channel description");
-					notificationChannel.enableLights(true);
-					notificationChannel.setLightColor(Color.RED);
-					notificationManager.createNotificationChannel(notificationChannel);
-					builder = new Notification.Builder(this, "my_notification_channel");
-				} else {
-					builder = new Notification.Builder(this);
-				}
-				List<SchoolMenu> ex = Listsave.SaveSchool.get_meal_month(today.getMonth() + 1);
+        if (data.size() > 0) {
+            if (data.containsKey("type")) {
+                NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                Intent intent = new Intent(this, MainActivity.class);
+                PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+                Notification.Builder builder;
+                String meal[] = new String[0];
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    NotificationChannel notificationChannel = new NotificationChannel("my_notification_channel", "My Notifications", NotificationManager.IMPORTANCE_DEFAULT);
+                    notificationChannel.setDescription("Channel description");
+                    notificationChannel.enableLights(true);
+                    notificationChannel.setLightColor(Color.RED);
+                    notificationManager.createNotificationChannel(notificationChannel);
+                    builder = new Notification.Builder(this, "my_notification_channel");
+                } else {
+                    builder = new Notification.Builder(this);
+                }
+                List<SchoolMenu> ex = Listsave.SaveSchool.get_meal_month(today.getMonth() + 1);
 
-				builder.setSmallIcon(android.R.drawable.star_on);
-				builder.setContentText("식단");
-				builder.setWhen(System.currentTimeMillis());
-				builder.setSmallIcon(R.mipmap.ic_launcher);
-				builder.setContentIntent(pendingIntent);
-				builder.setAutoCancel(true);
-				if (data.get("type").equals("조식")) { //아침 알람
+                builder.setSmallIcon(android.R.drawable.star_on);
+                builder.setContentText("식단");
+                builder.setWhen(System.currentTimeMillis());
+                builder.setSmallIcon(R.mipmap.ic_launcher);
+                builder.setContentIntent(pendingIntent);
+                builder.setAutoCancel(true);
+                if (data.get("type").equals("조식")) { //아침 알람
 
-					builder.setContentTitle("조식 알림");
-					meal = ex.get(today.getDate() - 1).lunch.split("dses\n");
+                    builder.setContentTitle("조식 알림");
+                    meal = ex.get(today.getDate() - 1).lunch.split("dses\n");
 
-				} else if (data.get("type").equals("중식")) { //점심 알람
+                } else if (data.get("type").equals("중식")) { //점심 알람
 
-					builder.setContentTitle("중식 알림");
-					meal = ex.get(today.getDate() - 1).lunch.split("\n");
+                    builder.setContentTitle("중식 알림");
+                    meal = ex.get(today.getDate() - 1).lunch.split("\n");
 
-				} else if (data.get("type").equals("석식")) { //저녁 알람
-					builder.setContentTitle("석식 알림");
-					meal = ex.get(today.getDate() - 1).lunch.split("\n");
+                } else if (data.get("type").equals("석식")) { //저녁 알람
+                    builder.setContentTitle("석식 알림");
+                    meal = ex.get(today.getDate() - 1).lunch.split("\n");
 
-				}
-				Notification.InboxStyle inboxStyle = null;
-				if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN) {
-					inboxStyle = new Notification.InboxStyle(builder);
+                }
+                Notification.InboxStyle inboxStyle = null;
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN) {
+                    inboxStyle = new Notification.InboxStyle(builder);
 
-					for (int i = 0; i < meal.length; i++)
-						inboxStyle.addLine(meal[i]);
-					inboxStyle.setSummaryText("전체 보기");
-					builder.setStyle(inboxStyle);
-					notificationManager.notify(0, builder.build());
-				}
+                    for (int i = 0; i < meal.length; i++)
+                        inboxStyle.addLine(meal[i]);
+                    inboxStyle.setSummaryText("전체 보기");
+                    builder.setStyle(inboxStyle);
+                    notificationManager.notify(0, builder.build());
+                }
 
 
-			} else if (data.containsKey("month")) {// 일정알람
-				String year = data.get("year"); // 년
-				String month = data.get("month").length() == 1 ? "0" + data.get("month") : data.get("month"); // 월
-				String date = data.get("date").length() == 1 ? "0" + data.get("date") : data.get("date"); // 월 // 일
-				String title = remoteMessage.getNotification().getTitle(); //제목
-				String msg = remoteMessage.getNotification().getBody(); //메세지
-				Listsave.HomeCardList.add(title, year + "." + month + "." + date + ".", msg);
-				sendNotification();
-			}
-		}
-	}
-	private void sendNotification(){
-		NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-		Intent go_main = new Intent(this, MainActivity.class);
-		PendingIntent contentIntent = PendingIntent.getActivity(this, 0, go_main, PendingIntent.FLAG_UPDATE_CURRENT);
-		Log.e("send","noti");
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-			NotificationChannel notificationChannel = new NotificationChannel("my_notification_channel", "My Notifications", NotificationManager.IMPORTANCE_DEFAULT);
+            } else if (data.containsKey("month")) {// 일정알람
+                String year = data.get("year"); // 년
+                String month = data.get("month").length() == 1 ? "0" + data.get("month") : data.get("month"); // 월
+                String date = data.get("date").length() == 1 ? "0" + data.get("date") : data.get("date"); // 월 // 일
+                String title = remoteMessage.getNotification().getTitle(); //제목
+                String msg = remoteMessage.getNotification().getBody(); //메세지
+                Listsave.HomeCardList.add(title, year + "." + month + "." + date + ".", msg);
+                sendNotification();
+            }
+        }
+    }
 
-			notificationChannel.setDescription("Channel description");
-			notificationChannel.enableLights(true);
-			notificationChannel.setLightColor(Color.RED);
-			nm.createNotificationChannel(notificationChannel);
-		}
-		NotificationCompat.Builder builder = new NotificationCompat.Builder(this,"my_notification_channel");
-		builder.setContentTitle("SNS 알림")
-				.setContentText("새로운 공지가 추가되었습니다.")
-				.setSmallIcon(R.drawable.bellxxxhdpi)
-				.setContentIntent(contentIntent)
-				.setAutoCancel(true)
-				.setDefaults(Notification.DEFAULT_ALL)
-				.setOngoing(true);
-		nm.notify(0, builder.build());
-	}
+    private void sendNotification() {
+        NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        Intent go_main = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, go_main, PendingIntent.FLAG_UPDATE_CURRENT);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel("my_notification_channel", "My Notifications", NotificationManager.IMPORTANCE_DEFAULT);
+
+            notificationChannel.setDescription("Channel description");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            nm.createNotificationChannel(notificationChannel);
+        }
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "my_notification_channel");
+        builder.setContentTitle("SNS 알림")
+                .setContentText("새로운 공지가 추가되었습니다.")
+                .setSmallIcon(R.drawable.bellxxxhdpi)
+                .setContentIntent(contentIntent)
+                .setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setOngoing(true);
+        nm.notify(0, builder.build());
+    }
 }
